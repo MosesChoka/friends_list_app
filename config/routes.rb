72423d1aad73bc682285be922ace5dcb4f801fd8
MC <chokamoses2@gmail.com>
2023-12-16 22:26:39 +0300

    Styled layouts, add friends db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :friends
+  root 'page#home'
   get 'page/home'
+  get 'page/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
